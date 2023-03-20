f59dbd013e4f83fe879e28f819700d27af8789a5
nekoify <xl83dev@gmail.com>
2023-03-20 21:37:03 +1000

    start on menu

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,29 @@
 <script lang="ts">
+    import Chat from "./lib/Chat.svelte";
+    import ChatMenu from "./lib/ChatMenu.svelte";
+    import Menu from "./lib/Menu.svelte";
+    import SignIn from "./lib/SignIn.svelte";
+    import SignUp from "./lib/SignUp.svelte";
+    import { Router, Route } from "svelte-navigator";
 </script>
 
 <main>
-  
+    <Router>
+        <Route path="/">
+            <Menu></Menu>
+        </Route>
+        <Route path="chatMenu">
+            <ChatMenu></ChatMenu>
+        </Route>
+        <Route path="chat">
+            <Chat></Chat>
+        </Route>
+        <Route path="signIn">
+            <SignIn></SignIn>
+        </Route>
+        <Route path="signUp">
+            <SignUp></SignUp>
+        </Route>
+    </Router>
 </main>
 
-<style>
-  
-</style>
---
+++ src/lib/Menu.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+</script>
+
+<h1>Chat app built with Svelte and Socket.io</h1>
+<button>Sign Up</button>
+<br>
+<button>Sign In</button>
+<br>
+<button>Chat Menu</button>
+
+<style>
+    button {
+        margin-bottom: 10px !important;
+    }
+</style>
