ddcbcfaa74c9e28d39bcb220648233f3aefee2dc
xl83 <xl83dev@gmail.com>
2023-03-21 02:23:29 +0000

    lots of stuff

---
+++ server/server.ts
@@ -0,0 +1,61 @@
+import express from 'express';
+import { Server } from "socket.io";
+import * as mongoose from "mongoose";
+import * as dotenv from 'dotenv'
+import * as http from 'http'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { sha256 } from 'js-sha256';
+
+//why is it so hard to load a .env file
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const envPath = __dirname.replace("server", "") + ".env"
+dotenv.config({path: envPath})
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.urlencoded())
+app.use(express.json())
+
+interface IFuser extends mongoose.Document {
+  username: string;
+  passwordHash: string;
+}
+
+const Userschema = new mongoose.Schema({
+  username: { type: String, required: true },
+  passwordHash: { type: String, required: true },
+});
+
+const User = mongoose.model<IFuser>("User", Userschema);
+
+
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('server');
+});
+
+io.on('connection', async(socket) => {
+  console.log("user connected")
+  socket.on('signup', async(data) => {
+  var hash = sha256(data.password)
+  User.create({username: data.username, passwordHash: hash})
+})
+})
+
+server.listen(3000, () => {
+    console.log('[Server] Websocket server connected on port 3000');
+})
+
+mongoose.connect(process.env.URL as string).then(() => {
+    console.log("[Server] Successfully connected to database");
+  }).catch( (err: any) => {
+    console.error(err)
+  })
--- src/lib/SignUp.svelte
+++ src/lib/SignUp.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 import { io } from "socket.io-client";
+var isConnected
 const socket = io("https://3000-nekoify-chatapp-yrjlbpenmgc.ws-us90.gitpod.io/", {
     reconnection: true,
+    transports: ['websocket']
 })
 function submit() {
     const username = (<HTMLInputElement>document.getElementById('username')).value
@@ -10,7 +12,9 @@ function submit() {
     alert(socket.connected)
     socket.emit("signup", {"username": username, "password": password})
 }
-
+socket.on("connect_error", (err) => {
+  console.log(`connect_error due to ${err.message}`);
+});
 </script>
 
 <h1>Create a account</h1>
