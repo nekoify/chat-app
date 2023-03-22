fa2ca07b6b77b96a20451fa0d6ac530717f3cc24
xl83 <xl83dev@gmail.com>
2023-03-22 09:33:08 +0000

    starting chatmenu

---
+++ src/lib/ChatMenu.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+</script>
+<h1>Chat Menu</h1>
+<h2>Current Amount of Users Online: 0</h2>
+<h3>List of users:</h3>
+<p>Placeholder (online)</p>
+<p>Placeholder (online)</p>
+<p>Placeholder (online)</p>
+<p>Placeholder (chatting with someone else)</p>
+<p>Placeholder (in room)</p>
