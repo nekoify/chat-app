30aab508c7054664900e68bc5aecd88b66167abc
xl83 <xl83dev@gmail.com>
2023-03-23 03:43:43 +0000

    starting on chat menu

---
+++ src/lib/User.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export var userInfo
+
+    function chat() {
+        console.log(userInfo)
+    }
+</script>
+{#if userInfo.inRoom == false || userInfo.inChat == false}
+    <p>{userInfo.username} (Chatting)</p>
+{:else if userInfo.inRoom == true  }
+<p>{userInfo.username} (In room)</p>
+{:else if userInfo.inChat == true } 
+<p>{userInfo.username} (Chatting to someone)</p>
+{/if}
+
+
+<button on:click={chat}>chat</button>
+<br>
+
+<style>p, button {
+    display: inline-block;
+}</style>
